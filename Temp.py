ed53924467994f0e5e2e0ff063a95aabd5429c3b
danielAA <rutp6vmp@yahoo.com.tw>
2023-05-09 15:02:50 +0800

    Initial commit

---
+++ Temp.py
@@ -0,0 +1,69 @@
+#!/usr/bin/python
+
+# -*- coding: utf-8 -*-
+
+
+import serial
+
+# import modbus_tk
+
+import modbus_tk.defines as cst
+
+import modbus_tk.modbus_rtu as modbus_rtu
+
+import modbus_tk.modbus_tcp as modbus_tcp
+
+from struct import *
+
+import json
+
+import requests
+
+import time
+
+import sqlite3
+
+import sys
+
+
+def poll_th(mb_id=1, port='/dev/ttyUSB0', timeout=1000, br=9600, databit=8, parity='N', stopbit=1):
+    print('-%d- poll_adtek_sun' % (mb_id))
+
+    data = {'time': time.strftime("%Y-%m-%d %H:%M:%S"), 'T': 0, 'H': 0}
+
+    mb_port = serial.Serial(port=port, baudrate=br, bytesize=databit, parity=parity, stopbits=stopbit)
+
+    master = modbus_rtu.RtuMaster(mb_port)
+
+    master.set_timeout(timeout / 1000.0)
+
+    try:
+
+        addr = 1
+
+        rr = master.execute(mb_id, cst.ANALOG_INPUTS, addr, 2)
+
+        print('rr: ', rr)
+
+        data['T'] = rr[0] / 10.0
+
+        data['H'] = rr[1] / 10.0
+
+
+
+    except Exception as e:
+
+        print("poll Error: " + str(e))
+
+    master._do_close()
+
+    return data
+
+
+def main():
+    data = poll_th()
+    print(data)
+
+
+if __name__ == '__main__':
+    main()
